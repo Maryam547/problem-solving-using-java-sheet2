f35039df1b8cb806d3c6b2a85af980a78a5e091e
SE-MaryamOsama <111062065+Maryam547@users.noreply.github.com>
2024-10-27 18:35:37 +0300

    Update M-LuckyNumbers.java

--- M-LuckyNumbers.java
+++ M-LuckyNumbers.java
@@ -1,1 +1,29 @@
+import java.util.Scanner;
+public class LuckyNumbers {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        int A = myObj.nextInt();
+        int B = myObj.nextInt();
+        boolean found = false;
+        for(int i=A;i<=B;i++){
+            int num = i;
+            boolean isLucky=true;
+            while(num>0){
+            int digit = num%10;
+            if(digit!=4&&digit!=7){
+                isLucky=false;
+                break;
+            }
+            num/=10;
+        }
+        if(isLucky){
+            System.out.print(i+" ");
+            found=true;
+        }
+    }
+    if(!found){
+        System.out.println(-1);
+    }
+  }
+}
 

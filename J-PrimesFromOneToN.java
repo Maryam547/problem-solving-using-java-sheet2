10fe681918ff22bb8f6cfce62b40e497c4ccf138
SE-MaryamOsama <111062065+Maryam547@users.noreply.github.com>
2024-10-27 17:15:18 +0300

    Update J-PrimesFromOneToN.java

--- J-PrimesFromOneToN.java
+++ J-PrimesFromOneToN.java
@@ -1,1 +1,19 @@
-
+import java.util.Scanner;
+public class PrimesFromOneToN {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        int N = myObj.nextInt();
+        for(int i=2;i<=N;i++){
+            boolean isPrime=true;
+            for(int j=2;j<=Math.sqrt(i);j++){
+                if(i%j==0){
+                    isPrime=false;
+                    break;
+                }
+            }
+            if(isPrime){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}

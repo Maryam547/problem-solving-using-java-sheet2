f0b69526c6ecef846736aa4ece0717953dc17444
SE-MaryamOsama <111062065+Maryam547@users.noreply.github.com>
2024-10-27 12:43:19 +0300

    Update D-FixedPassword.java

--- D-FixedPassword.java
+++ D-FixedPassword.java
@@ -1,1 +1,18 @@
+import java.util.Scanner;
+
+public class FixedPassword {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        
+        while(true){
+            int X = myObj.nextInt();
+            if (X == 1999) {
+                System.out.println("Correct");
+                break;
+            } else {
+                System.out.println("Wrong");
+            }
+        }
+    }
+}
 

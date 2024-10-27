b4886bcc97bbed50963e5e0026f52b46bde9fdfb
SE-MaryamOsama <111062065+Maryam547@users.noreply.github.com>
2024-10-27 12:53:53 +0300

    Update F-Multiplicationtable.java

--- F-Multiplicationtable.java
+++ F-Multiplicationtable.java
@@ -1,1 +1,12 @@
+import java.util.Scanner;
+
+public class MultiplicationTable {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        int N = myObj.nextInt();
+        for (int i = 1; i <= 12; i++) {
+            System.out.println(N+" * "+i+" = "+i*N);
+        }
+    }
+}
 

c864dceca27a22c20b31893bcbd2cb31ae5ffb6e
SE-MaryamOsama <111062065+Maryam547@users.noreply.github.com>
2024-10-27 14:42:39 +0300

    Update I-Plaindrome.java

--- I-Plaindrome.java
+++ I-Plaindrome.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+public class Plaindrome {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        int X = myObj.nextInt();
+        int orginalNum = X;
+        int reversedNum = 0;
+        while(X!=0){
+            int digit = X%10;
+            reversedNum = reversedNum*10+digit;
+            X/=10;
+        }
+        if(orginalNum==reversedNum){
+            System.out.println(reversedNum);
+            System.out.println("YES");
+        }else{
+            System.out.println(reversedNum);
+            System.out.println("NO");
+        }
+    }
+}
 

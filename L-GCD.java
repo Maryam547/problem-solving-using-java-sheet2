61f0f5d5d0cbbab20de44760a4c957918efb3f65
SE-MaryamOsama <111062065+Maryam547@users.noreply.github.com>
2024-10-27 17:59:49 +0300

    Update L-GCD.java

--- L-GCD.java
+++ L-GCD.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
+public class GCD {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        int A = myObj.nextInt();
+        int B = myObj.nextInt();
+        while(B!=0){
+            int temp=B;
+            B=A%B;
+            A=temp;
+        }
+        System.out.println(A);
+    }
+}
 

9a5e24795477f5999652771d5e12b64139af9b1f
SE-MaryamOsama <111062065+Maryam547@users.noreply.github.com>
2024-10-27 12:10:22 +0300

    Update A-OneToN.java

--- A-OneToN.java
+++ A-OneToN.java
@@ -1,1 +1,10 @@
-
+import java.util.Scanner;
+public class OneToN {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        int N = myObj.nextInt();
+        for(int i=1;i<=N;i++){
+            System.out.println(i);
+        }
+    }
+}

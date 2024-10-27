d96d325d205be4f05fb7ec353d6b52f8c39d59d1
SE-MaryamOsama <111062065+Maryam547@users.noreply.github.com>
2024-10-27 17:18:41 +0300

    Update K-Divisors.java

--- K-Divisors.java
+++ K-Divisors.java
@@ -1,1 +1,12 @@
+import java.util.Scanner;
+public class Divisors {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        int N = myObj.nextInt();
+        for(int i=1;i<=N;i++){
+            if(N%i==0)
+            System.out.println(i);
+        }
+    }
+}
 

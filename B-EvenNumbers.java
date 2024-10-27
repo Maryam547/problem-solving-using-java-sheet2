e0a2aeca0959df4982de7be080404948e313ebd1
SE-MaryamOsama <111062065+Maryam547@users.noreply.github.com>
2024-10-27 12:15:11 +0300

    Update B-EvenNumbers.java

--- B-EvenNumbers.java
+++ B-EvenNumbers.java
@@ -1,1 +1,14 @@
-
+import java.util.Scanner;
+public class EvenNumbers {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        int N = myObj.nextInt();
+        for(int i=1;i<=N;i++){
+            if(i%2==0){
+                System.out.println(i);
+            }else if(N==1){
+                System.out.println(-1);
+            }
+        }
+    }
+}

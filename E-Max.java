076e9ddd86df2b84c78fce33eb0b1bab872a85a2
SE-MaryamOsama <111062065+Maryam547@users.noreply.github.com>
2024-10-27 12:49:54 +0300

    Update E-Max.java

--- E-Max.java
+++ E-Max.java
@@ -1,1 +1,15 @@
+import java.util.Scanner;
+
+public class Max {
+    public static void main(String[] args) {
+        Scanner myObj = new Scanner(System.in);
+        int N = myObj.nextInt();
+        int max=0;
+        for (int i = 1; i <= N; i++) {
+            int X = myObj.nextInt();
+            max=Math.max(X,max);
+        }
+        System.out.println(max);
+    }
+}
 
